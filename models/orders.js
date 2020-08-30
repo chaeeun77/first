d539dafe5048773bdbfbba7d21fd5f3a47cdc987
chaeeun <chaeeun77@naver.com>
2020-08-30 16:43:43 +0900

    add order model

--- models/orders.js
+++ models/orders.js
@@ -2,7 +2,17 @@
 const mongoose = require('mongoose');
 
 //2번
-const orderSchema = new mongoose.Schema()
+const orderSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product', //reference를 product model 로 했으므로 product id로 타입을 지정하겠다!
+        required : true
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    }
+})
 
 //3번
 module.exports = mongoose.model("order", orderSchema)
