cc88a7b785234fccaadba9024e63da5915ad2062
chaeeun <chaeeun77@naver.com>
2020-08-30 22:51:29 +0900

    user sign up #3 (이메일 중복체크)

--- models/user.js
+++ models/user.js
@@ -2,7 +2,20 @@
 const mongoose = require('mongoose')
 
 //2번
-const userSchema = mongoose.Schema()
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
 
 //3번
 module.exports = mongoose.model("user", userSchema)
